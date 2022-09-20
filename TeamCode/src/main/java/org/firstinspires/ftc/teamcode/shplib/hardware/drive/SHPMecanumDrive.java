f9d5c4492c30f9fb4fdd54a0f9acc67a95572c2f
Aarav Chandrasekar <aaravc@gmail.com>
2022-09-19 21:50:22 -0700

    Drive Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/hardware/drive/SHPMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/hardware/drive/SHPMecanumDrive.java
@@ -11,20 +11,41 @@ public class SHPMecanumDrive extends SHPFourWheelDrive {
         super(hardwareMap, deviceNames);
     }
 
-    public void mecanum(double leftY, double leftX, double rightX) {
+    public void mecanum(double drive, double strafe, double turn) {
+
+        /*double angle = Math.atan2(drive, strafe) + imu.getYaw(); //angles.firstAngle
+        drive = drive * Math.cos(angle) + strafe * Math.sin(angle);
+        strafe = -drive * Math.sin(angle) + strafe * Math.cos(angle);*/
+
+        //strafe = strafe*Math.cos(imu.getYaw())-drive*Math.sin(imu.getYaw());
+        //drive = strafe*Math.sin(imu.getYaw()) + drive*Math.cos(imu.getYaw());
+
         double[] powers = {
-                //left rear
-                leftY + leftX + rightX,
+
                 //left front
-                leftY + leftX - rightX,
+                drive + strafe + turn,
+                //left back
+                drive - strafe + turn,
                 // right front
-                leftY - leftX + rightX,
-                //right rear
-                leftY - leftX - rightX,
+                drive - strafe - turn,
+                //right back
+                drive + strafe - turn,
         };
 
         setAll(powers);
+
+
+                /*//left rear
+                drive - strafe + turn,
+                //left front
+                drive + strafe + turn,
+                // right front
+                drive - strafe - turn,
+                //right rear
+                drive + strafe - turn, */
+
     }
+
 
     public void tankanum(double leftY, double rightY, double rightX) {
         double[] powers = {
